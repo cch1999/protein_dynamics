e2be01f69658595d7e5686a78822d59961ead287
Charlie <charlieharris001@gmail.com>
2021-04-22 15:58:14 +0100

    dist mat

--- keops.py
+++ keops.py
@@ -1,13 +1,18 @@
 import torch
 x = torch.randn(1000, 3, requires_grad=True)
-y = torch.randn(1000, 3)
 
 # Turn our Tensors into KeOps symbolic variables:
 from pykeops.torch import LazyTensor
-x_i = LazyTensor( x[:,None,:] )  # x_i.shape = (1e6, 1, 3)
-y_j = LazyTensor( y[None,:,:] )  # y_j.shape = ( 1, 2e6,3)
+
+xyz_i = LazyTensor(x[:,None,:] )  # x_i.shape = (1e6, 1, 3)
+xyz_j = LazyTensor(x[None,:,:] )  # y_j.shape = ( 1, 2e6,3)
 
 # We can now perform large-scale computations, without memory overflows:
-D_ij = ((x_i - y_j)**2).sum(dim=2)
+D_ij = ((xyz_i - xyz_j)**2).sum(-1).sqrt()
+V_ij = xyz_i - xyz_j
+
+print(D_ij)
+
+D_ij = D_ij < 4.0
 
 print(D_ij)
